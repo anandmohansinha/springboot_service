b0d54bba0b06f3ad7d33e591f7291481b914565d
anandmohansinha <anandsinhaknit@gmail.com>
2024-02-19 20:22:06 +0530

    initial commit

---
+++ demo2-rest-server/src/main/java/com/spring/rest/server/Demo2RestServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rest.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2RestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2RestServerApplication.class, args);
+	}
+
+}
---
+++ demo2-rest-server/src/test/java/com/spring/rest/server/Demo2RestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rest.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2RestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
